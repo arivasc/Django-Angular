dcb8521a315bbd7f93ec80eaf349c505d2435adc
Anthony Rivas <arivasc@unsa.edu.pe>
2021-08-06 07:01:42 -0500

    Creado modelo movie, serialiser de movie

--- api/views.py
+++ api/views.py
@@ -1,13 +1,12 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from api.serializers import UserSerializer
+from api.serializers import MovieSerializer
+from .models import Movie
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+class MovieViewSet(viewsets.ModelViewSet):
+
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
     permission_classes = [permissions.IsAuthenticated]
