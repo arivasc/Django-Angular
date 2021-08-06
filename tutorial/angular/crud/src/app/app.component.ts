5219e25f2d07dfe6f7c92702114ca1819887131b
Anthony Rivas <arivasc@unsa.edu.pe>
2021-08-06 07:29:51 -0500

    En angular creamos los metodos para obtener los datos desde django, pero django aun no envia esos datos.

--- tutorial/angular/crud/src/app/app.component.ts
+++ tutorial/angular/crud/src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService],
 })
 export class AppComponent {
   title = 'crud';
   movies = [{title: 'Titanic'}, {title:'avatar'}];
+
+  constructor(private api:ApiService){
+    this.getMovies();
+  }
+  getMovies = () => {
+    this.api.getAllMovies().subscribe(
+      data => {
+        this.movies = data;
+      },
+      error => {
+        console.log(error);
+      }
+    )
+
+  }
 }
